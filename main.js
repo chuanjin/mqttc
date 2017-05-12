cf0ada3f74bafbdfa0980b1ff15b521d04a2f385
Chuan Jin <chuan.jin.813@gmail.com>
2017-05-12 15:35:57 +0200

    Add separate js file

---
+++ main.js
@@ -0,0 +1,117 @@
+var connection = new Vue({
+    el: '#connection',
+    data: {
+        host: '',
+        port: '',
+        status: false,
+        statusIcon: 'fa fa-times',
+        clientId: '',
+        cleanSession: true,
+        username: '',
+        password: '',
+        notification: 'No Connection',
+        willTopic: '',
+        willPayload: '',
+        willQoS: 0,
+        willRetain: false,
+        pubTopic: '',
+        pubQoS: 0,
+        retain: false,
+        payload: '',
+        subTopic: '',
+        subQoS: 0,
+        subAck: '',
+        disabledBtn: true,
+        items: []
+    },
+    methods: {
+        connect: function() {
+            client = new Paho.MQTT.Client(this.host, Number(this.port), this.clientId)
+            var options = {
+                userName: this.username,
+                password: this.password,
+                cleanSession: this.cleanSession,
+                onSuccess: onConnect,
+                onFailure: onFailure
+            }
+            if (this.willTopic != '') {
+                will = new Paho.MQTT.Message(this.willPayload)
+                will.destinationName = this.willTopic
+                will.qos = Number(this.willQoS)
+                will.retained = this.willRetain
+                options.willMessage = will
+            }
+            client.onConnectionLost = onConnectionLost;
+            client.onMessageArrived = onMessageArrived;
+            client.connect(options);
+        },
+        subscribe: function() {
+            var options = {
+                qos: this.subQoS,
+                onSuccess: onSubSuccess,
+                onFailure: onSubFailure
+            };
+            client.subscribe(this.subTopic, options);
+        },
+        publish: function() {
+            client.send(this.pubTopic, this.payload, Number(this.pubQoS), this.retain)
+        },
+        toggleCS: function() {
+            this.cleanSession = !this.cleanSession
+        },
+        toggleRetain: function() {
+            this.retain = !this.retain
+        },
+        toggleWillRetain: function() {
+            this.willRetain = !this.willRetain
+        }
+    }
+})
+
+function updateValues(status, notification) {
+    connection.notification = notification
+    connection.status = status
+    if (status) {
+        connection.statusIcon = "fa fa-check"
+        connection.disabledBtn = false
+    } else {
+        connection.statusIcon = "fa fa-times"
+        connection.subAck = ""
+        connection.disabledBtn = true
+    }
+}
+
+function onConnect() {
+    updateValues(true, "Connected")
+}
+
+function onFailure(message) {
+    updateValues(false, "Fail to Connect")
+    console.log(message)
+}
+
+function onConnectionLost(responseObject) {
+    updateValues(false, "Connection Lost")
+    if (responseObject.errorCode !== 0) {
+        console.log("onConnectionLost:" + responseObject.errorMessage);
+    }
+}
+
+function onMessageArrived(message) {
+    var topic = message.destinationName;
+    var payload = message.payloadString;
+    connection.items.unshift({
+        topic: topic,
+        payload: payload
+    })
+}
+
+function onSubFailure(message) {
+    connection.subAck = "Fail to subscribe!"
+    console.log(message)
+}
+
+function onSubSuccess(message) {
+    connection.subAck = "Subscribe!"
+    console.log(message)
+}
