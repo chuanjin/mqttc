1e46344d3bfab7398dba75fffcbbe0f8b18a19b1
Chuan Jin <chuan.jin.813@gmail.com>
2017-05-17 11:00:24 +0200

    Add ID generator

--- main.js
+++ main.js
@@ -126,6 +126,9 @@ var connection = new Vue({
         },
         toggleWillRetain: function() {
             this.willRetain = !this.willRetain
+        },
+        generateID: function() {
+            this.clientId = createGuid()
         }
     }
 })
@@ -176,4 +179,13 @@ function onSubFailure(message) {
 function onSubSuccess(message) {
     connection.subAck = "Subscribe!"
     console.log(message)
+}
+
+
+function createGuid()
+{
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
 }
